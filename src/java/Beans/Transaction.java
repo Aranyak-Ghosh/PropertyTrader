8559f16196458e74fa4d505fb8fe65fa496e6b12
Aranyak-Ghosh <aranyakghosh@gmail.com>
2019-04-30 23:23:12 +0400

    Added Transaction Bean

---
+++ src/java/Beans/Transaction.java
@@ -0,0 +1,58 @@
+package Beans;
+
+import java.awt.Image;
+import java.io.Serializable;
+import javax.inject.Named;
+import javax.enterprise.context.Dependent;
+import javax.faces.bean.ManagedBean;
+
+/**
+ *
+ * @author yaseenfarooqui
+ */
+@ManagedBean(name = "transaction")
+public class Transaction implements Serializable {
+
+    private int propertyID;
+    private int transactionID;
+    private String buyerName;
+    private String sellerName;
+
+    public Transaction(){
+
+    }
+
+    public int getPropertyID() {
+        return propertyID;
+    }
+
+    public void setPropertyID(int propertyID) {
+        this.propertyID = propertyID;
+    }
+
+    public int getTransactionID() {
+        return transactionID;
+    }
+
+    public void setTransactionID(int transactionID) {
+        this.transactionID = transactionID;
+    }
+
+    public String getBuyerName() {
+        return buyerName;
+    }
+
+    public void setBuyerName(String buyerName) {
+        this.buyerName = buyerName;
+    }
+
+    public String getSellerName() {
+        return sellerName;
+    }
+
+    public void setSellerName(String sellerName) {
+        this.sellerName = sellerName;
+    }
+    
+    
+}
